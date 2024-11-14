3a7f75eaa45e8c8883c9c5aedd0eabc3b1a8b5a6
Matthew Hagemann <matt.hagemann@canonical.com>
2024-11-14 22:20:33 +0200

    chore: suppress warnings from bindings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 // Include the generated bindings
+
+#![allow(improper_ctypes)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 include!("bindings/bindings.rs");
 
 use std::ptr;
